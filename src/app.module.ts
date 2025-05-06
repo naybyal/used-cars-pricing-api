9bd4fa10a3be68c05cf3bf8af732c7cc22e2df88
naybyal <officiallynabiel@gmail.com>
2025-05-06 20:08:59 +0530

    feat: created user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities:[User],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
