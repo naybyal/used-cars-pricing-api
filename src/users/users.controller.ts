854b14f3b9494a283a53ecfe11a3fe9c4628dbad
naybyal <officiallynabiel@gmail.com>
2025-05-07 12:12:23 +0530

    feat: save data to db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,24 @@
 import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+
+    constructor(
+        // The constructor is used to inject dependencies into the class.
+        // In this case, we are injecting the UsersService.
+        private usersService: UsersService
+    ) {}
+
     @Post('/signup')
     createUser(
         @Body() body: CreateUserDto
     ) {
-        console.log(body);
+        // The createUser method is used to handle the POST request to the /signup endpoint.
+        // It takes a CreateUserDto object as the request body.
+        // It calls the create method of the UsersService to create a new user.
+        // The @Body() decorator is used to extract the request body and map it to the CreateUserDto object.
+        this.usersService.create(body.email, body.password);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @InjectRepository(User)     // Inject the User repository (dependency injection)
+        private repo: Repository<User>
+    ) {
+        // The constructor is used to inject dependencies into the class.
+        // In this case, we are injecting the User repository.
+    }
+
+    create(
+        email: string,
+        password: string
+    )  {
+         const user = this.repo.create({ email, password });
+            // The create method is used to create a new user object.
+            // It takes an object with the email and password properties.
+        return this.repo.save(user);
+    }
+}
