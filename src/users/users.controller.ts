97648eab611f96d0d7d2b7091d0cfbb8df3ac7f5
naybyal <officiallynabiel@gmail.com>
2025-05-10 23:07:57 +0530

    feat: custom decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,48 +25,75 @@ export class UsersController {
     constructor(
         private usersService: UsersService,
         private authService: AuthService
-    ) {}
-
-    @Get('/colors/:color')
-    setColor(
-        @Param('color') color: string,
-        @Session() session: any
     ) {
-        session.color = color;
     }
 
-    @Get('/colors')
-    getColor(
+
+    // @Get('/whoami')
+    // whoAmI(
+    //     @Session() session: any
+    // ) {
+
+    //     return this.usersService.findOne(session.userId);
+    // }
+
+
+    @Get('/whoami')
+    whoAmI(
         @Session() session: any
     ) {
-        return session.color;
+
+        return this.usersService.findOne(session.userId);
     }
+    
 
     @Post('/signup')
-    createUser(
-        @Body() body: CreateUserDto
+    async createUser(
+        @Body() body: CreateUserDto,
+        @Session() session: any
     ) {
-        return this.authService.signup(
+
+        const user = await this.authService.signup(
             body.email,
             body.password
         );
+
+        session.userId = user.id;
+
+        return user;
     }
 
     @Post('/signin')
-    signin(
-        @Body() body: CreateUserDto
+    async signin(
+        @Body() body: CreateUserDto,
+        @Session() session: any
     ) {
-        return this.authService.signin(
+
+        const user = await this.authService.signin(
             body.email,
             body.password
         );
+
+        session.userId = user.id;
+
+        return user;
+    }
+
+    @Post('/signout')
+    signout(
+        @Session() session: any
+    ) {
+        
+        session.userId = null;
+        return { message: 'User signed out!' };
     }
 
     @Get('/:id')
     async findUser(
         @Param('id') id: string 
     ) {
-        console.log("Handler is running...")
+
+        // console.log("Handler is running...")
         const user = await this.usersService.findOne(parseInt(id));
         if (!user)
             throw new NotFoundException('User not found!');
@@ -77,6 +104,7 @@ export class UsersController {
     findAllUsers(
         @Query('email') email: string
     ) {
+
         return this.usersService.find(email);
     }
 
@@ -84,6 +112,7 @@ export class UsersController {
     removeUser(
         @Param('id') id: string
     ) {
+
         return this.usersService.remove(parseInt(id));
     }
 
@@ -92,6 +121,7 @@ export class UsersController {
         @Param('id') id: string,
         @Body() body: UpdateUserDto
     ) {
+
         return this.usersService.update(parseInt(id), body);
     }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -25,7 +25,9 @@ export class UsersService {
     findOne(
         id: number
     ) {
-
+        
+        if (!id)
+            return null;
         return this.repo.findOneBy({ id });
     }
 
