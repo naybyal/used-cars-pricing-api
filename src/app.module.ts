461556b7c7b27710bc22b51f2debe85b2003aa79
naybyal <officiallynabiel@gmail.com>
2025-05-13 11:40:49 +0530

    feat: test db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,36 +3,56 @@ import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 import { Report } from './reports/report.entity';
+import { join } from 'path';
 
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities:[User, Report],
-    synchronize: true,
-  })],
-  controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_PIPE,
-    useValue: new ValidationPipe({
-      whitelist: true,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-  }],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME') || join(__dirname, '..', 'data', 'database.sqlite'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(cookieSession({
-        keys: [process.env.COOKIE_SECRET || 'akn5830ADK843'],
-        maxAge: 24 * 60 * 60 * 1000, // 24 hours
-      }),
-    )
-    .forRoutes('*');
+      .apply(
+        cookieSession({
+          keys: [process.env.COOKIE_SECRET || 'akn5830ADK843'],
+          maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        })
+      )
+      .forRoutes('*');
   }
 }
